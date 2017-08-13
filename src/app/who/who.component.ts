3668b76dca39acf6d03954d5b16fc0103152f124
daniSanlu <dani.sanlu@blackdata.systems>
2017-08-13 19:41:00 +0200

    [Add] Who - text

--- src/app/who/who.component.ts
+++ src/app/who/who.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MdSnackBar } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-who',
@@ -7,5 +10,25 @@ import {Component} from '@angular/core';
 })
 export class WhoComponent {
 
-    constructor() { }
+    /**
+     * @desc
+     *  Who form.
+     * @type {FormGroup}
+     */
+    whoForm = this.fb.group({
+        whoName   : ['', Validators.required],
+        whoText   : ['', Validators.required]
+    });
+
+    /**@todo: Save information in server.
+     * @desc
+     *  Callback to send information about someone.
+     */
+    onSendWho() {
+        // TODO: Server request. POST who.
+        this.snackBar.open(this.translateService.instant('FB.SUCCESS.SEND_GREET', {name: this.whoForm.get('whoName').value}));
+        this.whoForm.reset();
+    }
+
+    constructor(public fb: FormBuilder, public snackBar: MdSnackBar, public translateService: TranslateService) { }
 }
